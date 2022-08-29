a77bc3fa977f269c204764bcac2af4e8aac58ab5
38067 <dzepik0411@gmail.com>
2022-08-29 13:37:26 +0300

    Finish home work 004

--- Home work 002.py
+++ Home work 002.py
@@ -1,11 +1,13 @@
+from math import sqrt
+
 print('Hello, teacher!')
 a = 1
 b = 5
 c = 4
-d = -b + (b**2 - 4 * a * c) ** 0.5
-x_1 = d/2*a
+d = -b + sqrt(pow(b,2) - (4 * a * c))
+x_1 = d/(2*a)
 print(x_1)
 
-e = -b - (b**2 - 4 * a * c) ** 0.5
-x_2 = e/2*a
+e = -b - sqrt(pow(b,2) - (4 * a * c))
+x_2 = e/(2*a)
 print(x_2)
