0ab2dc2349019e103fc2bff2143bb862928b7c8a
38067 <dzepik0411@gmail.com>
2022-08-31 14:18:10 +0300

    Home Work 004

---
+++ lesson_3_hw_1/Home Work 004.py
@@ -0,0 +1,11 @@
+data = input('').split("*")
+name = data[0]
+birth_year = int(data[1].split("-")[0])
+death_year = int(data[2].split("-")[0])
+age = death_year - birth_year
+print(
+    f"""
+Name: {name}
+Age: {age} years
+"""
+)
