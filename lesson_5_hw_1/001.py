2e5459c20b9debc078488291d65004b38979b02b
38067 <dzepik0411@gmail.com>
2022-09-04 20:18:13 +0300

    Finish hw 001

---
+++ lesson_5_hw_1/001.py
@@ -0,0 +1,11 @@
+def check(n):
+    if (n < 2):
+        return (n % 2 == 0)
+    return (check(n - 2))
+
+
+n = int(input("Введите число: "))
+if (check(n) == True):
+    print("Число четное!")
+else:
+    print("Число нечетное!")
