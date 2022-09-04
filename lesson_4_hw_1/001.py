aca6e02408477a03d57a700118c8825c38a5996d
38067 <dzepik0411@gmail.com>
2022-09-04 20:49:40 +0300

    Correct hw 001

--- lesson_4_hw_1/001.py
+++ lesson_4_hw_1/001.py
@@ -3,14 +3,14 @@ import math
 
 def degrees2radians(degrees):
     n_rad = (float(degrees)*math.pi)/180
-    return math.cos(n_rad)
+    return (n_rad)
 
 
 a = degrees2radians(60)
 b = degrees2radians(45)
 c = degrees2radians(40)
 
-print(a, b ,c)
+print(math.cos(a), math.cos(b) ,math.cos(c))
 
 
 
