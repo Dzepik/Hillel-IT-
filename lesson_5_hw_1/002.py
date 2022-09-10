0920da9d797111a432c498649f70dcdf70b744b5
38067 <dzepik0411@gmail.com>
2022-09-10 17:25:44 +0300

    correct 002

--- lesson_5_hw_1/002.py
+++ lesson_5_hw_1/002.py
@@ -1,26 +1,26 @@
 #  функцію для рішення квадратних рівнянь
 
+import math
 
+
+print('Введите коэффициенты для уравнения')
+print('ax^2 + bx + c = 0')
 a = float(input('Введите a: '))
 b = float(input('Введите b: '))
 c = float(input('Введите c: '))
 
 def solve_quadratic_equation(a, b, c):
     D = b ** 2 - 4 * a * c
-    if D < 0:
-        print('Дискриминант = 0')
-        print('Решений нет')
+
+    if D > 0:
+        x1 = (-b + math.sqrt(D)) / (2 * a)
+        x2 = (-b - math.sqrt(D)) / (2 * a)
+        print("x1 = %.2f \nx2 = %.2f" % (x1, x2))
     elif D == 0:
-        x = (-b + D ** .5) / (2 * a)
-        print('Дискриминант =', D)
-        print('Корень один: ', x)
+        x = -b / (2 * a)
+        print("x = %.2f" % x)
     else:
-        x_1 = (-b + D ** .5) / (2 * a)
-        x_2 = (-b - D ** .5) / (2 * a)
-        print('Дискриминант =', D)
-        print('Есть два корня: ')
-        print('Корень 1 =  ', x_1)
-        print('Корень 2 =  ', x_2)
+        print("Корней нет")
 
 
 solve_quadratic_equation(a, b, c)
