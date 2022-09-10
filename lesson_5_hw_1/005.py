311bd0ce9404bb86f1e8492352489372c6c7eb13
38067 <dzepik0411@gmail.com>
2022-09-10 17:00:59 +0300

    correct 005

--- lesson_5_hw_1/005.py
+++ lesson_5_hw_1/005.py
@@ -1,13 +1,17 @@
 #   Для введеного числа x виводить значення sign(x)
 
-x = int(input('Введіть число: '))
 
-if x > 0:
-    print('sing(x) = 1')
-elif x < 0:
-    print('sign(-x) = -1')
-else:
-    print('sign(x) = 0')
+def sing(x):
+    if x > 0:
+        print('sing(x) = 1')
+    elif x < 0:
+        print('sign(-x) = -1')
+    else:
+        print('sign(x) = 0')
+
+
+x = int(input('Введіть число: '))
+sing(x)
 
 
 
