4b492a1239717903f3d43aafafac6adf8a97099d
38067 <dzepik0411@gmail.com>
2022-09-13 00:38:05 +0300

    Finish 002

--- lesson_5_hw_1/002.py
+++ lesson_5_hw_1/002.py
@@ -15,12 +15,13 @@ def solve_quadratic_equation(a, b, c):
     if D > 0:
         x1 = (-b + math.sqrt(D)) / (2 * a)
         x2 = (-b - math.sqrt(D)) / (2 * a)
-        print("x1 = %.2f \nx2 = %.2f" % (x1, x2))
+        print('x1 = %.2f \nx2 = %.2f' % (x1, x2))
     elif D == 0:
-        x = -b / (2 * a)
-        print("x = %.2f" % x)
+        x1= -b / (2 * a)
+        print('x = %.2f' % x)
     else:
-        print("Корней нет")
+        print('Корней нет')
+    return D
 
 
 solve_quadratic_equation(a, b, c)
