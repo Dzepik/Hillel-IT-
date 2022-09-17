4f5267f0e9c0a583e4ae136b5d55277f0e836976
38067 <dzepik0411@gmail.com>
2022-09-17 20:14:15 +0300

    005

--- lesson_6_hw_1/005.py
+++ lesson_6_hw_1/005.py
@@ -2,5 +2,29 @@
 Задача
 """
 
+coords = {}
+alphabet = "abcdefgh"
+count = 1
+for i in range(8):
+    for j in range(1, 9):
+        coords[count] = alphabet[i] + str(j)
+        count += 1
 
+
+def calculate_wheat_chess_position(kilograms: float) -> str:
+    seed_weight = 35
+    seed_count = int(kilograms * 1000000 / seed_weight)
+    num = 1
+    step = 1
+    while num < seed_count:
+        if step == 1:
+            num = 2
+        else:
+            num **= 2
+        step += 1
+    return coords[step]
+
+kilograms = 0.03584
+
+print(calculate_wheat_chess_position(kilograms))
 
