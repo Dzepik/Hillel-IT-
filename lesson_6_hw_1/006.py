9371c1a75e0cab98adce0885e66b0e54c53908d6
38067 <dzepik0411@gmail.com>
2022-09-17 21:44:27 +0300

    006

--- lesson_6_hw_1/006.py
+++ lesson_6_hw_1/006.py
@@ -3,33 +3,20 @@
 що знаходяться між двома заданими символами включно.
 """
 
-#def sum_symbol_codes():
-    #a = ord('a')
-    #b = ord('b')
-    #c = ord('c')
-    #d = ord('d')
-    #result = a + b + c + d
-
-
-    #return result
-
-#print(sum_symbol_codes())
-
-
-
 def sum_symbol_codes():
-    my_list = list(map(ord, 'abcdefghijklmnopqrstuvwxyz'))
-    first = ord('a')
-    last = ord('d')
-    while first < last:
-        first = first + 1
-    #result = my_list[0] + my_list[1] + my_list[2] + my_list[3]
-    return first
+    a = ord('a')
+    b = ord('b')
+    c = ord('c')
+    d = ord('d')
+    result = a + b + c + d
 
 
-
+    return result
 
 print(sum_symbol_codes())
+
+
+
 
 
 
