8efed77fb415f7db57df22135e2f813fb24c6609
38067 <dzepik0411@gmail.com>
2022-09-22 16:00:28 +0300

    001

---
+++ lesson_8_hw_1/001.py
@@ -0,0 +1,9 @@
+"""
+функція index(lst, elem), що повертатиме індекс elem в списку lst, або,
+якщо елемент відсутній, то None.
+"""
+
+lst = [1, 3, 7, 11, 67, 444]
+
+for elem in enumerate(lst):
+    print(elem)
